5c302fc7aa0d544fa9e030a37972bbe23f83f331
kk <dileep2malviya@gmail.com>
2026-07-31 00:42:01 +0530

    user auth 1.6

--- backend/services/user/src/db/index.ts
+++ backend/services/user/src/db/index.ts
@@ -3,7 +3,7 @@ import {DB_NAME} from "../constants.js"
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://Admin-1234:Admin-1234@cluster0.1ceelo6.mongodb.net/${DB_NAME}` as string)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}` as string)
         console.log(`Mongodb connect !! DB HOST : ${connectionInstance.connection.host}`)
     } catch (error: unknown) {
         console.log("ERROR",process.env.MONGO_URL)
